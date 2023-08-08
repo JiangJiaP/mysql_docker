6db1d3f3bc8d62434e86e2161b39045f32793fcc
jiaajiang <jiaajiang@tencent.com>
2023-08-08 17:53:40 +0800

    d

--- init.sql
+++ init.sql
@@ -3,6 +3,3 @@ CREATE TABLE id_info (
     route_id VARCHAR(255) NOT NULL,
     multi_id VARCHAR(255) NOT NULL
 );
-
-INSERT INTO users (username, password) VALUES ('user1', 'password1');
-INSERT INTO users (username, password) VALUES ('user2', 'password2');
