2aa992f2bdfcbb7e46e2861d9c38361c60be1f54
jiaajiang <jiaajiang@tencent.com>
2023-09-14 17:39:21 +0800

    d

--- init.sql
+++ init.sql
@@ -1,5 +1,6 @@
 CREATE TABLE id_info (
     cid VARCHAR(1024) NOT NULL,
     route_id VARCHAR(1024) NOT NULL,
-    multi_id VARCHAR(1024) NOT NULL
+    multi_id VARCHAR(1024) NOT NULL,
+    mac_id VARCHAR(1024) NOT NULL
 );
